2e57e470f358216faeb804a673399fbf2c79e45d
sreejith07255 <sreejithtr07225@gmail.com>
2024-06-25 11:03:00 +0530

    Add files via upload
    blinked led using hardware

---
+++ sketch_jun25a.ino
@@ -0,0 +1,13 @@
+int led=5;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led,HIGH);
+delay(3000);
+digitalWrite(led,LOW);
+delay(3000);
+}
